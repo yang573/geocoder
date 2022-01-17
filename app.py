272eb8bcf09d3ff55c857fa5efa0f5d8e78bd7fc
Yang Yang <yangyang1998@gmail.com>
2022-01-16 19:22:59 -0500

    Structured queries

--- app.py
+++ app.py
@@ -2,6 +2,45 @@ import csv, sys
 import requests
 from typing import Tuple, List
 
+class Address:
+    def __init__(self,
+            number: str,
+            street: str,
+            city: str = "",
+            county: str = "",
+            state: str = "",
+            country: str = "US",
+            postalcode: str = ""):
+        self.street_number = number
+        self.street_name = street
+        self.city = city
+        self.county = county
+        self.state = state
+        self.country = country
+        self.postalcode = postalcode
+
+    def to_query(self):
+        # turn class properties into a dictionary
+        # skips all empty strings
+        query = {k: v for k, v in self.__dict__ if bool(v)}
+
+        # fix street param
+        query["street"] = "{} {}".format(self.street_number, self.street_name)
+        del query["street_number"]
+        del query["street_name"]
+
+        return query
+
+def geocode_address(addr: Address) -> Tuple[str, str]:
+    params = addr.to_query()
+    params["format"] = "json"
+    params["limit"] = 1
+
+    resp = requests.get("https://nominatim.openstreetmap.org/search", params=params)
+    json = resp.json()
+    print(resp)
+    return (json[0]['lat'], json[0]['lon'])
+
 def geocode_query(addr: str) -> Tuple[str, str]:
     params = {
         "q": addr,
@@ -13,6 +52,9 @@ def geocode_query(addr: str) -> Tuple[str, str]:
     json = resp.json()
     print(json)
     return (json[0]['lat'], json[0]['lon'])
+
+def read_csv(filename: str) -> List[Address]:
+    return
 
 def read_unstructured_csv(filename: str) -> List[str]:
     lines = None
@@ -27,6 +69,9 @@ def geocode_unstructured_csv(filename: str):
     for addr in addrs:
         x, y = geocode_query(addr)
         print(x, y)
+    return
+
+def geocode_csv(filename: str):
     return
 
 if __name__ == "__main__":
